8bbac634755d7701815e10bf23f1745e8de2c4b3
vagrant <vagrant@precise32.(none)>
2015-02-06 10:49:38 +0000

    Putting it Together Lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
+var index = require('./routes/index');
+var add = require('./routes/add');
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	res.render('add');
+	var newFriend=
+	{
+		'name': req.query.name,
+		'description': req.query.description,
+		'imageURL' : 'http://lorempixel.com/400/400/people'
+	};
+	data["friends"].push(newFriend);
  }
